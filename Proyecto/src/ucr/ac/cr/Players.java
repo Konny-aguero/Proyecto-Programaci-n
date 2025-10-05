8ea3a1f3bd157eccaea5ea56400dff024a2b51f7
Konny-aguero <konnyaguero@gmail.com>
2025-10-05 01:39:22 -0600

    Add files via upload

---
+++ Proyecto/src/ucr/ac/cr/Players.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package ucr.ac.cr;
+
+public class Players {
+    private String nameArmy;
+
+    public String getNameArmy() {
+        return this.nameArmy;
+    }
+
+    public void setNameArmy(String nameArmy) {
+        this.nameArmy = nameArmy;
+    }
+}
---
+++ Proyecto/src/ucr/ac/cr/Wizard.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package ucr.ac.cr;
+
+public class Wizard extends Hero {
+    public Wizard(int hp) {
+        super(hp);
+    }
+
+    public void move() {
+    }
+
+    public void attack() {
+    }
+}
