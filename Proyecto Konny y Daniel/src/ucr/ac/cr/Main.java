1d82da1970d8982afa1377774e0faa3b61f06b3f
Konny Aguero <konnyaguero@gmail.com>
2025-10-05 02:01:57 -0600

    prueba

--- Proyecto Konny y Daniel/src/ucr/ac/cr/Main.java
+++ Proyecto Konny y Daniel/src/ucr/ac/cr/Main.java
@@ -5,6 +5,7 @@ public class Main extends Board {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean flag;
+        //xd
         showMainMenu();
         int option = sc.nextInt();
 
