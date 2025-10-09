3ca8828caaeaa5b895a362df237446aa7e7068c9
danielgomezso <danielgsolano27@gmail.com>
2025-10-09 12:48:10 -0600

    Hola

--- Proyecto Konny y Daniel/src/ucr/ac/cr/Board.java
+++ Proyecto Konny y Daniel/src/ucr/ac/cr/Board.java
@@ -1,18 +1,9 @@
 package ucr.ac.cr;
-
 import java.util.Scanner;
+import static ucr.ac.cr.GameConfig.*;
 
 public class Board {
-    static final String EMPTY = "[ ]";
-//    public static final String COLOR_RED = "\u001B[31m";
-//    private static final String PLAYER_1_COLOR = COLOR_RED;
-//    private static final String PLAYER_2_COLOR = COLOR_BLUE;
-//    public static void printColor(String texto, String color) {
-//        System.out.println(color + texto + COLOR_RESET);
-//    }
 
-    public static final String COLOR_BLUE = "\u001B[34m";
-    public static final String COLOR_RESET = "\u001B[0m";
 
     static String[][] board;
     static void initBoard(int size) {
